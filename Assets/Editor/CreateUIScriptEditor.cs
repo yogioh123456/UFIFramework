9424302c4d0b6a7fcd6b45987566e640976ee08d
Unknown <991655778@qq.com>
2018-11-06 11:08:19 +0800

    初始化
    添加FGUI  ILRuntime

---
+++ Assets/Editor/CreateUIScriptEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateUIScriptEditor : MonoBehaviour
+{
+    [MenuItem("Assets/创建UI脚本", priority = 0)]
+    static void CreateUI(){
+        string[] guidArray = Selection.assetGUIDs;
+        foreach (var item in guidArray)
+        {
+            //打印路径
+            string selecetFloder = AssetDatabase.GUIDToAssetPath(item);
+            Debug.Log(selecetFloder);
+            //打印名字
+            string selectName = Path.GetFileName(selecetFloder);
+            Debug.Log(selectName);
+        }
+    }
+}
