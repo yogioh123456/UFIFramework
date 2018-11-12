ae408c148aa821de6f41a5e6873de2a551bcb3e9
Unknown <991655778@qq.com>
2018-11-12 19:16:45 +0800

    添加读取excel功能

--- Assets/Test/TestCS.cs
+++ Assets/Test/TestCS.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Data;
+using ExcelDataReader;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System;
+
 
 public class TestCS : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //读取excel
+        GameReadExcel("/Test/Excel/testExcel.xlsx");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// 读取excel表
+    /// </summary>
+    /// <param name="path">Path.</param>
+    void GameReadExcel(string path){
+        FileStream stream = File.Open(Application.dataPath + path, FileMode.Open, FileAccess.Read);
+        IExcelDataReader excelDataReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        DataSet result = excelDataReader.AsDataSet();
+        int cols = result.Tables[0].Columns.Count;
+        int rows = result.Tables[0].Rows.Count;
+        //一行一行地读取
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                string v = result.Tables[0].Rows[i][j].ToString();
+                Debug.Log(v);
+            }
+        }
     }
 }
